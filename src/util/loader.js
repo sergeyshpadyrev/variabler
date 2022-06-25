4ecb3dd969d9e318ba48a49d2891e0f0e95a6002
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2022-06-25 20:17:29 +0300

    Make loader from vault work

--- src/util/loader.js
+++ src/util/loader.js
@@ -1,3 +1,4 @@
+const { executeCommand } = require('./executor')
 const { logError } = require('./logger')
 
 const isString = value => typeof value === 'string' || value instanceof String
@@ -6,12 +7,16 @@ const loadVariables = (config, key) => {
   const value = config[key]
   if (!isString(value)) return value
 
-  const provider = value.split('://')[0]
+  const valueParts = value.split('://')
+  const provider = valueParts[0]
+  const path = valueParts[1]
 
   switch (provider) {
     case 'vault':
-      // TODO add vault loading here
-      return {}
+      const vaultCommandResponse = executeCommand(`vault kv get -format=json ${path}`)
+      const vaultData = JSON.parse(vaultCommandResponse).data.data
+
+      return vaultData
 
     default:
       logError(`Invalid provider "${provider}"`)
