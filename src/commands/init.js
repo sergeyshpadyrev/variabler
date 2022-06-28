8d4e3a81dc4cf21495a4582895ac566978c03d35
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2022-06-28 15:17:22 +0300

    Remove unused imports

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,9 +1,7 @@
 const assertionService = require('../services/assertion.service')
-const { configPath, scriptPath } = require('../util/path')
-const { copyDirectory } = require('../util/files')
+const { configPath } = require('../util/path')
 const errors = require('../constants/errors')
 const gitService = require('../services/git.service')
-const initialData = require('../constants/initialData')
 const loggerService = require('../services/logger.service')
 const messages = require('../constants/messages')
 const templatesService = require('../services/templates.service')
@@ -11,8 +9,7 @@ const templatesConfigService = require('../services/templatesConfig.service')
 const variablesConfigService = require('../services/variablesConfig.service')
 
 module.exports = () => {
-  const path = configPath('.')
-  assertionService.assertNotExists(path, errors.alreadyInitialized)
+  assertionService.assertNotExists(configPath('.'), errors.alreadyInitialized)
 
   templatesService.init()
   templatesConfigService.init()
