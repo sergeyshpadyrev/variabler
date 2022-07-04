03eabe4d705e8e79f8a4c702847448ea1e95fe79
Sergey Shpadyrev <sergeyshpadyrev@gmail.com>
2022-07-04 18:55:18 +0300

    Add one more expect to init test

--- e2e/tests/init.test.js
+++ e2e/tests/init.test.js
@@ -7,10 +7,12 @@ describe('Init command ', () => {
     expectFileExists('variabler/config.json')
     expectFileNotExists('src/api.js')
     expectFileNotExists('settings.json')
+    expectFileNotExists('assets/icon.png')
 
     runInRepo('variabler set env:staging')
 
     expectFileExists('src/api.js')
     expectFileExists('settings.json')
+    expectFileExists('assets/icon.png')
   })
 })
